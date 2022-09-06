c04362231c6d6dc4cb6ffd77eefc3d4f6f560f60
Peter Vang <vang_peter06@yahoo.com>
2022-09-06 11:33:45 -0700

    initial add

---
+++ src/components/Ticket.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ticket(props) {
+  return (
+  <React.Fragment>
+    <h3>{props.location} - {props.names}</h3>
+    <p><em>{props.issue}</em></p>
+    </React.Fragment>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
+
+export default Ticket;
